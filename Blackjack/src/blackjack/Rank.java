4d05bb46271757e90e90e24ac68c123dbbfe7f95
Paul Pearson <pauldarlingtonpearson@gmail.com>
2017-07-06 21:44:11 -0600

    Signed-off-by: Paul Pearson <pauldarlingtonpearson@gmail.com>

--- Blackjack/src/blackjack/Rank.java
+++ Blackjack/src/blackjack/Rank.java
@@ -1,6 +1,6 @@
 package blackjack;
 
 public enum Rank {
-	ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
 
 }
